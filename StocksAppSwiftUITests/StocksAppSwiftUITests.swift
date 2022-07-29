7dd36a0973660fce183a0b19d094291b724a7261
Ghullam abbas <ghulam.abbas@sigmatec.com.pk>
2022-07-29 11:56:36 +0500

    add sample unit test

---
+++ StocksAppSwiftUITests/StocksAppSwiftUITests.swift
@@ -0,0 +1,30 @@
+//
+//  StocksAppSwiftUITests.swift
+//  StocksAppSwiftUITests
+//
+//  Created by Developer on 29/07/2022.
+//
+
+import XCTest
+
+class StocksAppSwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
