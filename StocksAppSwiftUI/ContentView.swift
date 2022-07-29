74ce4fc7045d050033dea939c5498c7386d4f400
Ghullam abbas <ghulam.abbas@sigmatec.com.pk>
2022-07-29 09:36:48 +0500

    first commit

---
+++ StocksAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksAppSwiftUI
+//
+//  Created by Developer on 28/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StocksAppSwiftUI/StocksAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppSwiftUIApp.swift
+//  StocksAppSwiftUI
+//
+//  Created by Developer on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
